40dd0baca418307c748fb2f8f1874eecc4dcb848
VujevicStipe <stipevujevicking@gmail.com>
2025-10-13 15:42:17 +0200

    hero component add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import HeroSection from "./sections/hero-section/HeroSection";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main></main>
+      <main>
+        <HeroSection />
+      </main>
     </div>
   );
 }
