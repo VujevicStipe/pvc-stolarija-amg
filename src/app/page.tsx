32235a89f2d1aa70a035ec660c606957568c48f8
VujevicStipe <stipevujevicking@gmail.com>
2025-10-15 14:55:55 +0200

    added benefits component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import HeroSection from "./sections/hero-section/HeroSection";
+import BenefitsSection from "./sections/benefits-section/BenefitsSection";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main>
         <HeroSection />
+        <BenefitsSection />
       </main>
     </div>
   );
