d65845baa2faef2bd1fda166b225009e309cc7b9
unknown <stipevujevic5@gmail.com>
2025-11-17 04:44:53 -0800

    added shocase section

---
+++ src/app/sections/showcase-section/ShowcaseSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import DoorSlider from "@/app/components/showcase-slider/ShowcaseSlider";
+
+export default function ShowcaseSection() {
+
+  return (
+    <section className="section">
+      <DoorSlider />
+    </section>
+  );
+}
