b194c469df6cdc85ca31f59f1541cdb8771ff57f
unknown <stipevujevic5@gmail.com>
2025-11-18 16:20:02 +0100

    new component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import styles from "./page.module.css";
 import HeroSection from "./sections/hero-section/HeroSection";
 import BenefitsSection from "./sections/benefits-section/BenefitsSection";
 import AboutSection from "./sections/about-section/AboutSection";
-import GallerySection from "./sections/gallery-section/GallerySection";
 import ShowcaseSection from "./sections/showcase-section/ShowcaseSection";
+import Gallery from "./components/gallery/Gallery";
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
         <BenefitsSection />
         <AboutSection />
         <ShowcaseSection />
-        <GallerySection />
+        <Gallery />
       </main>
     </div>
   );
