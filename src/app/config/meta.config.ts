7dd4a73f29c8e2bf59e9f3e362fa6600a92bdc8a
unknown <stipevujevic5@gmail.com>
2025-11-19 20:09:32 +0100

    added metadata

---
+++ src/app/config/meta.config.ts
@@ -0,0 +1,35 @@
+export const meta = {
+  url: 'https://www.amg-pvc.hr',
+  title: 'AMG PVC Stolarija',
+  name: 'AMG PVC Stolarija obrt',
+  description: 'Proizvodnja i montaža PVC prozora, vrata, balkonskih vrata i roletni u Splitu i okolici. Vrhunska kvaliteta, brza montaža, dugogodišnja garancija.',
+  keywords: 'PVC stolarija Split, PVC prozori Split, PVC vrata, montaža prozora, Kaštela, Dalmacija',
+  author: 'AMG PVC Stolarija',
+  company: {
+    name: 'AMG PVC Stolarija',
+    legalName: 'AMG Obrt za proizvodnju PVC stolarije',
+    address: {
+      street: 'Ulica Sv. Jurja 74',
+      city: 'Kaštel Stari',
+      region: 'Dalmacija',
+      postalCode: '21217',
+      country: 'Croatia',
+    },
+    geo: {
+      latitude: 43.567034,
+      longitude: 16.354090,
+    },
+    contact: {
+      phone: '+385 XX XXX XXXX',
+      email: 'amgpvcstolarija.bussines@gmail.com',
+    },
+    social: {
+      facebook: 'https://www.facebook.com/p/Amg-Pvcstolarija-100084518001943/',
+      instagram: 'https://www.instagram.com/amgpvc',
+    },
+    openingHours: {
+      weekdays: 'Monday,Tuesday,Wednesday,Thursday,Friday 08:00-16:00',
+      saturday: 'Saturday 09:00-13:00',
+    },
+  },
+} as const;
