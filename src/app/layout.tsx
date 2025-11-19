da515bc4ecc211c1f08b002ff57bca9541a7e1ae
unknown <stipevujevic5@gmail.com>
2025-11-19 20:06:12 +0100

    added metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import { getLocalizedJsonLd } from '../app/lib/BuildMetadata';
+import { meta } from '../app/config/meta.config';
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -9,13 +11,73 @@ const montserrat = Montserrat({
   weight: ["300", "400", "500", "600", "700"],
 });
 
-export default function RootLayout({
+export const metadata = {
+  metadataBase: new URL(meta.url),
+  title: {
+    default: meta.title,
+    template: `%s | ${meta.title}`,
+  },
+  description: meta.description,
+  keywords: meta.keywords,
+  authors: [{ name: meta.author }],
+  creator: meta.author,
+  publisher: meta.name,
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  verification: {
+    google: 'YOUR_GOOGLE_VERIFICATION_CODE',
+  },
+  alternates: {
+    canonical: meta.url,
+  },
+  openGraph: {
+    type: 'website',
+    locale: 'hr_HR',
+    url: meta.url,
+    siteName: meta.title,
+    title: meta.title,
+    description: meta.description,
+    images: [
+      {
+        url: `${meta.url}/meta/og-image.pngfigma`,
+        width: 1200,
+        height: 630,
+        alt: 'AMG PVC Stolarija - Proizvodnja prozora i vrata',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: meta.title,
+    description: meta.description,
+    images: [`${meta.url}/meta/og-image.png`],
+  },
+};
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const jsonLd = await getLocalizedJsonLd();
+
   return (
     <html lang="hr" className={montserrat.variable}>
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </head>
       <body>
         <header>
           <Navbar />
