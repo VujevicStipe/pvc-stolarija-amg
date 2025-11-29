7275a88202cb8208e30e03e1ee7d6cd7c45ee64f
unknown <stipevujevic5@gmail.com>
2025-11-29 13:58:05 +0100

    created robots file

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/api/',
+    },
+    sitemap: 'https://pvc-stolarija-amg.hr/sitemap.xml',
+  }
+}
